d242604fc6d9659e843fe18eaab7c0eef665cfb3
jcochoa234 <144067087+jcochoa234@users.noreply.github.com>
2024-05-19 20:07:30 -0700

    Backend repositories implementations

---
+++ NgAir/NgAir.BackEnd/Repositories/Implementations/CategoriesRepository.cs
@@ -0,0 +1,61 @@
+﻿using NgAir.BackEnd.Repositories.Interfaces;
+using NgAir.Shared.DTOs;
+using NgAir.Shared.Entities;
+using NgAir.Shared.Responses;
+
+namespace NgAir.BackEnd.Repositories.Implementations
+{
+    public class CategoriesRepository : GenericRepository<Category>, ICategoriesRepository
+    {
+        private readonly DataContext _context;
+
+        public CategoriesRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetComboAsync()
+        {
+            return await _context.Categories
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+        }
+
+        public override async Task<ActionResponse<IEnumerable<Category>>> GetAsync(PaginationDTO pagination)
+        {
+            var queryable = _context.Categories.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            return new ActionResponse<IEnumerable<Category>>
+            {
+                WasSuccess = true,
+                Result = await queryable
+                    .OrderBy(x => x.Name)
+                    .Paginate(pagination)
+                    .ToListAsync()
+            };
+        }
+
+        public override async Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination)
+        {
+            var queryable = _context.Categories.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            double count = await queryable.CountAsync();
+            int totalPages = (int)Math.Ceiling(count / pagination.RecordsNumber);
+            return new ActionResponse<int>
+            {
+                WasSuccess = true,
+                Result = totalPages
+            };
+        }
+    }
+}
---
+++ NgAir/NgAir.BackEnd/Repositories/Implementations/CitiesRepository.cs
@@ -0,0 +1,66 @@
+﻿using NgAir.BackEnd.Repositories.Interfaces;
+using NgAir.Shared.DTOs;
+using NgAir.Shared.Entities;
+using NgAir.Shared.Responses;
+
+namespace NgAir.BackEnd.Repositories.Implementations
+{
+    public class CitiesRepository : GenericRepository<City>, ICitiesRepository
+    {
+        private readonly DataContext _context;
+
+        public CitiesRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<City>> GetComboAsync(int stateId)
+        {
+            return await _context.Cities
+                .Where(c => c.StateId == stateId)
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+        }
+
+        public override async Task<ActionResponse<IEnumerable<City>>> GetAsync(PaginationDTO pagination)
+        {
+            var queryable = _context.Cities
+                .Where(x => x.State!.Id == pagination.Id)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            return new ActionResponse<IEnumerable<City>>
+            {
+                WasSuccess = true,
+                Result = await queryable
+                    .OrderBy(x => x.Name)
+                    .Paginate(pagination)
+                    .ToListAsync()
+            };
+        }
+
+        public override async Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination)
+        {
+            var queryable = _context.Cities
+                .Where(x => x.State!.Id == pagination.Id)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            double count = await queryable.CountAsync();
+            int totalPages = (int)Math.Ceiling(count / pagination.RecordsNumber);
+            return new ActionResponse<int>
+            {
+                WasSuccess = true,
+                Result = totalPages
+            };
+        }
+    }
+}
