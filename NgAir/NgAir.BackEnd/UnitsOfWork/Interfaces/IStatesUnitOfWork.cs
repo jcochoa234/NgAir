c2a41b70911bdc966cf4a64872a9c08829ec7a52
jcochoa234 <144067087+jcochoa234@users.noreply.github.com>
2024-05-19 19:38:57 -0700

    Backend units of work interfaces

---
+++ NgAir/NgAir.BackEnd/UnitsOfWork/Interfaces/IStatesUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using NgAir.Shared.DTOs;
+using NgAir.Shared.Entities;
+using NgAir.Shared.Responses;
+
+namespace NgAir.BackEnd.UnitsOfWork.Interfaces
+{
+    public interface IStatesUnitOfWork
+    {
+        Task<ActionResponse<State>> GetAsync(int id);
+
+        Task<ActionResponse<IEnumerable<State>>> GetAsync();
+
+        Task<ActionResponse<IEnumerable<State>>> GetAsync(PaginationDTO pagination);
+
+        Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination);
+
+        Task<IEnumerable<State>> GetComboAsync(int countryId);
+    }
+}
