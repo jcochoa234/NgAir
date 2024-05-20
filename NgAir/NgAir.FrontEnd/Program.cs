01b1b9f6362119033fc256ae003ebf30d7e879f7
Juan Carlos Ocha <juan.ochoa@sonorasoft.com>
2024-05-20 08:28:03 -0700

    Add autenthication

--- NgAir/NgAir.FrontEnd/Program.cs
+++ NgAir/NgAir.FrontEnd/Program.cs
@@ -1,11 +1,27 @@
+using Blazored.Modal;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using NgAir.FrontEnd;
+using NgAir.FrontEnd.AuthenticationProviderJWT;
+using NgAir.FrontEnd.Repositories;
+using NgAir.FrontEnd.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7278/") });
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredModal();
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<AuthenticationProviderJWT>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
 
 await builder.Build().RunAsync();
