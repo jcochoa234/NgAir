fbe8fad24da8bf449c9c3c3620790e3d78e6951a
jcochoa234 <144067087+jcochoa234@users.noreply.github.com>
2024-05-19 19:42:52 -0700

    Backend units of work implementations

---
+++ NgAir/NgAir.BackEnd/UnitsOfWork/Implementations/CategoriesUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using NgAir.BackEnd.UnitsOfWork.Interfaces;
+using NgAir.Shared.DTOs;
+using NgAir.Shared.Entities;
+using NgAir.Shared.Responses;
+
+namespace NgAir.BackEnd.UnitsOfWork.Implementations
+{
+    public class CategoriesUnitOfWork : GenericUnitOfWork<Category>, ICategoriesUnitOfWork
+    {
+        private readonly ICategoriesRepository _categoriesRepository;
+
+        public CategoriesUnitOfWork(IGenericRepository<Category> repository, ICategoriesRepository categoriesRepository) : base(repository)
+        {
+            _categoriesRepository = categoriesRepository;
+        }
+
+        public override async Task<ActionResponse<IEnumerable<Category>>> GetAsync(PaginationDTO pagination) => await _categoriesRepository.GetAsync(pagination);
+
+        public override async Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination) => await _categoriesRepository.GetTotalPagesAsync(pagination);
+
+        public async Task<IEnumerable<Category>> GetComboAsync() => await _categoriesRepository.GetComboAsync();
+    }
+}
